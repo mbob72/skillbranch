c07bb285da2a4273d0f84031ed720064d06cb3f8
Mike Bobryshev <mbob72@gmail.com>
2016-11-14 21:22:52 +0300

    refactor exersize 2C

--- index2C.js
+++ index2C.js
@@ -10,26 +10,7 @@ app.get('/', function(req, res) {
 	function showError() {
 		res.end('Invalid username');
 	}
-
-	function getFirstChar(word) {
-		return word[0].toUpperCase() + '.';
-	}
-
-	function fullToShort(resArr) {
-		if (resArr.length == 1) return;
-
-		if (resArr.length == 2) {
-			resArr.reverse();
-			resArr[1] = getFirstChar(resArr[1]);
-		}
-
-		if (resArr.length == 3) {
-			resArr.splice(0, 0, resArr.pop())
-			resArr[1] = getFirstChar(resArr[1]);
-			resArr[2] = getFirstChar(resArr[2]);
-		}
-	}
-
+	
 	var un = req.query.username;
 
 	if (!un) {
